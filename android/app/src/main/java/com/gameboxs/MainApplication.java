6520aa303fbaa2bb9534e23600d830c88c081bb2
misael <misaelzurdo@gmail.com>
2017-10-01 23:13:29 -0300

    Login facebook android

--- android/app/src/main/java/com/gameboxs/MainApplication.java
+++ android/app/src/main/java/com/gameboxs/MainApplication.java
@@ -2,16 +2,18 @@ package com.gameboxs;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -24,11 +26,16 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new VectorIconsPackage()
       );
     }
   };
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
@@ -37,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    AppEventsLogger.activateApp(this);
   }
 }
--- view/login.js
+++ view/login.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import FBSDK, {LoginButton, AccessToken} from 'react-native-fbsdk';
+import {Actions} from 'react-native-router-flux';
 
 export default class LoginlView extends Component {
 
     componentDidMount() {}
+
+    goToHome() {
+        Actions.home();
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>
+                <Text style={styles.walcome}>
                     GameBoxs
                 </Text>
+                <LoginButton
+                readPermissions={['public_profile', 'email']}
+                onLoginFinished={
+                    (error, result) => {
+                    if (error) {
+                        console.log("login has error: " + result.error);
+                    } else if (result.isCancelled) {
+                        this.goToHome();
+                        console.log("login is cancelled.");
+                    } else {
+                        AccessToken.getCurrentAccessToken().then(
+                        (data) => {
+                            console.log(data.accessToken.toString())
+                        }
+                        )
+                    }
+                    }
+                }
+          onLogoutFinished={() => alert("logout.")}/>
             </View>
         );
     }
@@ -19,6 +44,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'lightgray',
-        paddingTop: 10
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    walcome: {
+        fontSize: 20,
+        fontWeight: '600',
+        marginBottom: 20
     }
 });
